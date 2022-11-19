37e41fbdce8e1fc476e12da99d819aea9d4fd9d0
Alexandra Vega <alexandravega370@fpuna.edu.py>
2022-11-19 09:11:19 -0300

    ClienteRepository

--- src/main/java/com/gestion/fidelizacion/repositorios/ClienteRepository.java
+++ src/main/java/com/gestion/fidelizacion/repositorios/ClienteRepository.java
@@ -1,9 +1,10 @@
 package com.gestion.fidelizacion.repositorios;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.gestion.fidelizacion.entidades.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends PagingAndSortingRepository<Cliente, Long>{
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
 }
